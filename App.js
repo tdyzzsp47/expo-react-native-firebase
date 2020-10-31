16a536258e1585b271081e7114aa3fb52d54a3cb
tdyzzsp47 <tdyzzsp47@gmail.com>
2020-10-31 16:59:53 +0900

    mod: 不要な関数を削除

--- App.js
+++ App.js
@@ -7,17 +7,6 @@ import firebase from './firebase';
 import Auth from './components/Auth'
 
 export default function App() {
-
-  const auth = () => {
-    firebase.auth().signInAnonymously().then(user => {
-      console.log(user)
-    }).catch(error => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      console.log(error);
-    });
-  }
-
   return (
     <View style={styles.container}>
       <Auth />
